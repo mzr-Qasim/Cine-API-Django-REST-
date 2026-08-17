377c99954e400917ce0921dd6f07f63b88e31086
Muhammad Qasim <malikqasim20051@gmail.com>
2026-08-17 01:09:50 -0700

    Creating , Updating and Deleting Data in DRF

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -9,9 +9,16 @@ class MovieSerializer(serializers.Serializer):
     active = serializers.BooleanField()
     
     
-    def create(self, validated_data):
-        
+    def create(self, validated_data):   
         return Movie.objects.create(**validated_data)
+    
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
     
     
     
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -29,15 +29,27 @@ def movieList(request):
 
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def movieDetails(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
 
-
-
-
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+   
+        
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=204)
 
 
 
