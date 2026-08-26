66608da2bf4b6a4b16b9cd6fe896d0cbcb65c8ea
Muhammad Qasim <malikqasim20051@gmail.com>
2026-08-26 15:31:59 +0500

    feat: add Django admin login brute-force protection

--- cineAPI/settings.py
+++ cineAPI/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     'watchlist_app',
+    'axes',
 ]
 
 
@@ -60,7 +61,20 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'axes.middleware.AxesMiddleware',
 ]
+
+
+AUTHENTICATION_BACKENDS = [
+    # AxesStandaloneBackend should be the first backend in the AUTHENTICATION_BACKENDS list.
+    'axes.backends.AxesStandaloneBackend',
+
+    # Django ModelBackend is the default authentication backend.
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+
+
 
 ROOT_URLCONF = 'cineAPI.urls'
 
